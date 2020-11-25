09cca8baf9bf31510c3c413eb5786995fe4ec6d1
eeong <discography8@gmail.com>
2020-11-25 12:22:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+	#app.container.d-flex.flex-column 
+		nav-bar
+		footer-bar
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+	name: 'app',
+	components: {
+		'nav-bar': NavBar,
+		'footer-bar' : Footer,
+	}
+}
+</script>
+
+<style lang="scss">
+	$lightColor: #f0f1f4;
+	html,body,div,p,ul,li,h1,h2,h3,h4,h5 {
+		margin: 0; padding: 0; list-style: none;
+	}
+	#app {height: 100vh; border-left: 2px solid	$lightColor; border-right: 2px solid $lightColor; padding: 0 !important;}
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang='pug'>
+	footer.footer-wrapper.bg-dark.text-light.text-center
+		.p-3 &copy; eeong.com
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+	.footer{position: fixed; bottom: 0;}
+</style>
